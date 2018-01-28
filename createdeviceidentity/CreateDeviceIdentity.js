86aa9b387738e98dc4bc48927c63f34f21fd9560
veeravat <veeravat@me.com>
2018-01-28 18:07:12 +0700

    workshop file

---
+++ createdeviceidentity/CreateDeviceIdentity.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var iothub = require('azure-iothub');
+
+var connectionString = 'HostName=iothub-tuymove.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=kJtqZWw+l4Vvpdug1uEsCVzyVveL6swE2opb+sf8tNc=';
+
+var registry = iothub.Registry.fromConnectionString(connectionString);
+
+var device = {
+    deviceId: 'myFirstNodeDevice'
+}
+registry.create(device, function(err, deviceInfo, res) {
+    if (err) {
+        registry.get(device.deviceId, printDeviceInfo);
+    }
+    if (deviceInfo) {
+        printDeviceInfo(err, deviceInfo, res)
+    }
+});
+
+function printDeviceInfo(err, deviceInfo, res) {
+    if (deviceInfo) {
+        console.log('Device ID: ' + deviceInfo.deviceId);
+        console.log('Device key: ' + deviceInfo.authentication.symmetricKey.primaryKey);
+    }
+}
